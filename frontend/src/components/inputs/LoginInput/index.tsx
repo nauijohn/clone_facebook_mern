b050c9aa098a0fd3f9cc20b10a01d55228c1cd12
nauijohn <85679013+nauijohn@users.noreply.github.com>
2025-10-26 22:01:02 +1100

    login page

--- frontend/src/components/inputs/LoginInput/index.tsx
+++ frontend/src/components/inputs/LoginInput/index.tsx
@@ -1,4 +1,5 @@
 import { ErrorMessage, useField } from "formik";
+import { useMediaQuery } from "react-responsive";
 
 import styles from "./styles.module.css";
 
@@ -8,17 +9,28 @@ interface Props extends React.InputHTMLAttributes<HTMLInputElement> {
 
 const LoginInput = ({ bottom, ...props }: Props) => {
   const [field, meta] = useField(props);
+  const desktopView = useMediaQuery({
+    query: "(min-width: 850px)",
+  });
 
   return (
     <div className={styles.input_wrap}>
       {meta.touched && meta.error && !bottom && (
         <div
-          className={styles.input_error}
+          className={`${
+            desktopView
+              ? `${styles.input_error} ${styles.input_error_desktop}`
+              : styles.input_error
+          }`}
           style={{ transform: "translateY(3px)" }}
         >
           {meta.touched && meta.error && <ErrorMessage name={field.name} />}
           {meta.touched && meta.error && (
-            <div className={styles.error_arrow_top}></div>
+            <div
+              className={
+                desktopView ? styles.error_arrow_left : styles.error_arrow_top
+              }
+            ></div>
           )}
         </div>
       )}
@@ -32,12 +44,22 @@ const LoginInput = ({ bottom, ...props }: Props) => {
       />
       {meta.touched && meta.error && bottom && (
         <div
-          className={styles.input_error}
+          className={`${
+            desktopView
+              ? `${styles.input_error} ${styles.input_error_desktop}`
+              : styles.input_error
+          }`}
           style={{ transform: "translateY(2px)" }}
         >
           {meta.touched && meta.error && <ErrorMessage name={field.name} />}
           {meta.touched && meta.error && (
-            <div className={styles.error_arrow_bottom}></div>
+            <div
+              className={
+                desktopView
+                  ? styles.error_arrow_left
+                  : styles.error_arrow_bottom
+              }
+            ></div>
           )}
         </div>
       )}
@@ -45,7 +67,7 @@ const LoginInput = ({ bottom, ...props }: Props) => {
       {meta.touched && meta.error && (
         <i
           className={styles.error_icon}
-          style={{ top: `${!bottom && "63%"}` }}
+          style={{ top: `${!bottom && !desktopView ? "63%" : "15px"}` }}
         ></i>
       )}
     </div>
